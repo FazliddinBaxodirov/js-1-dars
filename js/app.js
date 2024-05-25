27d7c2212fbb4b1f17f2c4b19c4cee98d8689a13
FazliddinBaxodirov <baxodirovfazliddin670@gmail.com>
2024-05-25 21:17:51 +0500

    Finish project

---
+++ js/app.js
@@ -0,0 +1,61 @@
+var firstTask = prompt("HTML nimani anglatadi?");
+var secondTask = prompt("Eng katta sarlavha uchun to'g'ri HTML elementi?");
+var thirdTask = prompt("Satr uzilishini kiritish uchun to'g'ri HTML elementi nima?");
+var fourthTask = prompt("Yakuniy tegni ko'rsatish uchun qaysi belgi ishlatiladi?");
+var fivethTask = prompt("CSS nimani anglatadi?");
+var sixthTask = prompt("media,mixin,include,import dan oldin qaysi belgi qo'yiladi?");
+var seventhTask = prompt("Inline uslublarini aniqlash uchun qaysi HTML atributidan foydalaniladi?");
+var eighthTask = prompt("CSS da orqa fonni ozgartrish uchun qaysi xususiyatdan foydalaniladi?");
+var ninethTask = prompt("CSS da matn rangini ozgartrish uchun qaysi xususiyatdan foydalaniladi");
+var tenthTask = prompt("CSS da matn hajmini boshqaradigan xususiyat nomi?");
+var eleventhTask = prompt("Sarlavha teglari nechta?");
+var twelvethTask = prompt("<span> qanday element?");
+var thirteenthTask = prompt("Justify content qanday vazifani bajaradi?");
+var fourteenthTask = prompt("Gap qanday vazifani bajaradi?");
+var fifteenthTask = prompt("Class orqali style berish qanday nomlanadi?");
+var sixteenthTask = prompt("Section teglari qaysi semantik tegni ichida joylashgan bo'ladi?");
+var seventeenthTask = prompt("scss fayllarini bir biriga ulash uchun qaysi xususiyatdan foydalaniladi?");
+var eighteenthTask = prompt("meta tegi qanday vazifani bajaradi?");
+
+document.getElementById("demo1").innerHTML = firstTask;
+document.getElementById("demo2").innerHTML = secondTask;
+document.getElementById("demo3").innerHTML = thirdTask;
+document.getElementById("demo4").innerHTML = fourthTask;
+document.getElementById("demo5").innerHTML = fivethTask;
+document.getElementById("demo6").innerHTML = sixthTask;
+document.getElementById("demo7").innerHTML = seventhTask;
+document.getElementById("demo8").innerHTML = eighthTask;
+document.getElementById("demo9").innerHTML = ninethTask;
+document.getElementById("demo10").innerHTML = tenthTask;
+document.getElementById("demo11").innerHTML = eleventhTask;
+document.getElementById("demo12").innerHTML = twelvethTask;
+document.getElementById("demo13").innerHTML = thirteenthTask;
+document.getElementById("demo14").innerHTML = fourteenthTask;
+document.getElementById("demo15").innerHTML = fifteenthTask;
+document.getElementById("demo16").innerHTML = sixteenthTask;
+document.getElementById("demo17").innerHTML = seventeenthTask;
+document.getElementById("demo18").innerHTML = eighteenthTask;
+
+var userObj = {
+    task1:firstTask,
+    task2:secondTask,
+    task3:thirdTask,
+    task4:fourthTask,
+    task5:fivethTask,
+    task6:sixthTask,
+    task7:seventhTask,
+    task8:eighthTask,
+    task9:ninethTask,
+    task10:tenthTask,
+    task11:eleventhTask,
+    task12:twelvethTask,
+    task13:thirteenthTask,
+    task14:fourteenthTask,
+    task15:fifteenthTask,
+    task16:sixteenthTask,
+    task17:seventeenthTask,
+    task18:eighteenthTask
+}
+
+console.log(userObj);
+
